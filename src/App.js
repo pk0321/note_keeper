ba35ba16f1ea638101c3c16d858e0a9100a4d8c7
Manish Khanchandani <manishkk74@gmail.com>
2017-11-27 23:06:46 -0800

    fixing app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDom from 'react-dom';
 import NoteCard from "./NoteCard.js"
 import './App.css';
 
@@ -7,67 +6,63 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      notes: []
+      notes: [],
+	  noteText: '',
+	  noteTitle: '',
+	  displaySidebar: ''
     }
-    this.showSidebar = this.showSidebar.bind(this);
-    this.addNote = this.addNote.bind(this);
   }
-
-  showSidebar(e) {
-    e.preventDefault();
-    this.showSidebar.classList.toggle("show");
+  
+  toggleSideBar() {
+	  this.setState({displaySidebar: !this.state.displaySidebar});
   }
 
   addNote(e) {
     e.preventDefault();
-    //console.log("submitted");
-    //console.log(this);
-
-    const note = {
-      title: this.noteTitle.value,
-      text: this.noteText.value
-    };
-    const newNotes = Array.from(this.state.notes);
-    newNotes.push(note);
+    var myNotes = this.state.notes;
+	myNotes.push({text: this.state.noteText, title: this.state.noteTitle});
     this.setState({
-      notes: newNotes
+      notes: myNotes,
+	  noteText: '',
+	  noteTitle: ''
     });
-
-    // Clear the notes and hide sidebar
-    this.noteTitle.value = "";
-    this.noteText.value = "";
-    this.showSidebar();
+    this.toggleSideBar();
   }
 
 
   render() {
+	  var myClass = 'sidebar';
+	  if (this.state.displaySidebar) {
+		myClass = myClass + ' show';  
+	  }
+	  
     return (
       <div>
           <header className="mainHeader">
             <h1>Noted</h1>
             <nav>
-              <a href="" onClick={this.showSidebar}>Add New Note</a>
+              <a href="" onClick={(e) => {e.preventDefault(); this.toggleSideBar();}}>Add New Note</a>
             </nav>
           </header>
       
           <section className="notes">
             {this.state.notes.map((note,i) => {
               return (
-                <NoteCard note={note} key={'note-${i}'} />
+                <NoteCard note={note} key={i} />
               )
             })}
           </section>
 
-          <aside className="sidebar" ref={ref => this.showSidebar = ref}>
-            <form onSubmit={this.addNote}>
+          <aside className={myClass}>
+            <form onSubmit={this.addNote.bind(this)}>
               <h3>New Note</h3>
-              <div className="close-btn" onClick={this.showSidebar}>
+              <div className="close-btn" onClick={() => this.toggleSideBar()}>
                   <i className="fa fa-times"></i>
               </div>
               <label htmlFor="note-title">Title:</label>
-              <input type="text" name="note-title" ref = {ref => this.noteTitle = ref}/>
+              <input type="text" name="note-title" value={this.state.noteTitle} onChange={(e)=>{this.setState({noteTitle: e.target.value})}}/>
               <label htmlFor="note-text">Text:</label>
-              <textarea name="note-text" ref = {ref => this.noteText = ref}></textarea>
+              <textarea name="note-text" value={this.state.noteText} onChange={(e)=>{this.setState({noteText: e.target.value})}}></textarea>
               <input type="submit" value="Add New Note" />
             </form>                    
           </aside>
